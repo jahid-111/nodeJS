8f35fc203c01004cf6fb12a001be5091a8abf834
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-23 15:15:28 +0300

    handle-url

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const port = 8000;
 
 myServer = http.createServer((req, res) => {
+  //   if (req.url === "/favicon.ico") return req.end();
+
   const logReq = `=> user at ${Date.now()} time,   requested :${
     req.url
   } this route \n`;
+
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", logReq, (error, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("hello Server homePage");
         break;
       case "/about":
-        res.end("Hey It's me Jahid.  I'm Learning NodeJs");
+        res.end(`Hey It's me ${myUrl?.query?.name}.  I'm Learning NodeJs`);
         break;
       case "/contact":
         res.end("Email : jahidjob5@outlook.com");
+        break;
+      case "/search":
+        res.end(`You'r Searching about  ${myUrl.query.search_query} ?`);
         break;
       default:
         res.end(" 404 Not Found");
